8b3467f931a3a36562f5121324326790541e541e
Pavan8494 <144001512+Pavan8494@users.noreply.github.com>
2024-06-13 16:28:22 +0530

    Update Converters.h

--- Converters.h
+++ Converters.h
@@ -1,1 +1,20 @@
 
+boolean tryToConvertStringToInterger(const char* str, int* intergerValue) {
+    char* endptr;
+    boolean returnValue = TRUE;
+    *intergerValue = strtol(str, &endptr, 10);
+    if (endptr == str) {
+        returnValue = FALSE;
+    }
+    return returnValue;
+}
+
+boolean tryToConvertStringToDouble(const char* str, double* doubleValue) {
+    char* endptr;
+    boolean returnValue = TRUE;
+    *doubleValue = strtod(str, &endptr);
+    if (endptr == str) {
+        returnValue = FALSE;
+    }
+    return returnValue;
+}
